9951e2728e7d80908778dd70ac449ecd2a68ddc5
FlyingLexmi <159624193+FlyingLexmi@users.noreply.github.com>
2025-03-26 14:23:54 +0200

    Add files via upload

--- src/preloader.js
+++
@@ -1,7 +0,0 @@
-const play = document.getElementById("play");
-const playForm = document.getElementById("playForm");
-
-play.addEventListener("click", () => {
-    // Показываем iframe
-    playForm.style.display = "block"; // Используем block, чтобы iframe стал видимым
-});
